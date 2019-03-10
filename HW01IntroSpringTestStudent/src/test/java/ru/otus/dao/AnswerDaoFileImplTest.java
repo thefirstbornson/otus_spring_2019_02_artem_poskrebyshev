f86e2091faa0e25b9a8c5b2396068670df34dd8d
Artem <thefirstbornson@gmail.com>
2019-03-10 23:16:21 +0200

    hw3 test corrected

--- HW01IntroSpringTestStudent/src/test/java/ru/otus/dao/AnswerDaoFileImplTest.java
+++ HW01IntroSpringTestStudent/src/test/java/ru/otus/dao/AnswerDaoFileImplTest.java
@@ -4,21 +4,38 @@ package ru.otus.dao;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.otus.Main;
+import ru.otus.service.SequenceService;
+
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = Main.class)
+
+
 class AnswerDaoFileImplTest {
     @Autowired
     private AnswerDaoFileImpl dao;
+    @Autowired
+    @Qualifier("answerSequenceService")
+    private SequenceService answerSequenceService;
+
+    AnswerDaoFileImpl aDao= new AnswerDaoFileImpl("",answerSequenceService);
 
     @Test
     void parseCSV() throws IOException {
         assertTrue(dao.parseCSV().size() > 0);
     }
+
+    @Test
+    void TestNoFile(){
+        assertThrows(IOException.class, ()->aDao.parseCSV());
+    }
+
 }
