a794a8e4d5408268bb78b6788f2c84f83e176e01
Artem <thefirstbornson@gmail.com>
2019-03-11 12:30:50 +0200

    hw3 test correct spring boot logo

--- hwteststudent/src/test/java/ru/otus/dao/AnswerDaoFileImplTest.java
+++ hwteststudent/src/test/java/ru/otus/dao/AnswerDaoFileImplTest.java
@@ -2,23 +2,12 @@ package ru.otus.dao;
 
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
-import ru.otus.Main;
-import ru.otus.config.ConfigTest;
 import ru.otus.service.SequenceService;
-
 import java.io.IOException;
-
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
